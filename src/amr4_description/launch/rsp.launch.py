8bdeb0c238451a4fb9b2d5e053e7f786eab9a0ae
joshika2505 <joshika2505@gmail.com>
2026-09-02 12:02:14 +0530

    Enable dual DiffDrive 4-sprocket synchronous acceleration and resolve robot_description ParameterValue

--- src/amr4_description/launch/rsp.launch.py
+++ src/amr4_description/launch/rsp.launch.py
@@ -3,6 +3,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.substitutions import Command
 from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
     try:
@@ -12,7 +13,7 @@ def generate_launch_description():
         pkg_path = get_package_share_directory('amr4_description')
         xacro_file = os.path.join(pkg_path, 'urdf', 'amr4.urdf.xacro')
     
-    robot_description = Command(['xacro ', xacro_file])
+    robot_description = ParameterValue(Command(['xacro ', xacro_file]), value_type=str)
 
     return LaunchDescription([
         Node(
